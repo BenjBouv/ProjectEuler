c03ed6b2691dd8edf6cce8dbedec729a28205f92
Benjamin BOUVIER <bnjbouv+github@gmail.com>
2012-07-25 23:14:56 +0200

    Updated primes so as to be usable externally: you just need to import primes_until() and the table primes to use the module.

---
+++ finished/utils/primes.py
@@ -0,0 +1,49 @@
+# -*- coding:utf-8 -*-
+# Computes primes numbers with simple algorithm : checks all the
+# potentials prime divisors less than square root(n). The only values to
+# check and greater than 3 have the form 6n-1 or 6n+1, for n non zero
+# integer.
+from math import sqrt, ceil
+
+namePrimesFile = "primes.txt"
+
+primes = [2, 3]
+limit_processed = 3
+n1 = -1
+n2 = +1
+
+def check_is_prime(n):
+    limit = sqrt(n)
+    result = True
+    for p in primes:
+        if n % p == 0:
+            result = False
+            break
+        if p > limit:
+            break
+    return result
+
+def seek_primes(attempts):
+    global n1, n2, limit_processed
+    while attempts > 0:
+
+        while n2 <= limit_processed and n1 <= limit_processed:
+            attempts -= 1
+            n1 += 6
+            n2 += 6
+
+        if check_is_prime(n1):
+            primes.append(n1)
+        if check_is_prime(n2):
+            primes.append(n2)
+
+        limit_processed = n2
+
+def primes_until( limit ):
+    seek_primes( 1 + limit / 6 )
+
+def save():
+    file = open(namePrimesFile, "w+")
+    for p in primes:
+        file.write("%s " % p)
+    file.close()
